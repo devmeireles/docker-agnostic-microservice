c75a367d6bd4c746f6c83ac548a474bdd61703ca
Gabriel Santos Meireles <dev.meireles@gmail.com>
2023-03-02 19:40:01 +0000

    adding kafka connector

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -3,26 +3,34 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
+  Inject,
   Post,
   Response,
   ValidationPipe,
 } from '@nestjs/common';
+import { Producer } from 'kafkajs';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
-  private authService: AuthService;
 
-  constructor(authService: AuthService) {
-    this.authService = authService;
-  }
+  constructor(
+    private readonly authService: AuthService,
+    @Inject('KAFKA_PRODUCER')
+    private kafkaProducer: Producer,
+  ) { }
 
   @Post('/signup')
-  signUp(
+  async signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
-    return this.authService.signUp(authCredentialsDto);
+    this.authService.signUp(authCredentialsDto);
+
+    await this.kafkaProducer.send({
+      topic: 'register-topic',
+      messages: [{ key: 'register', value: JSON.stringify(authCredentialsDto) }],
+    });
   }
 
   @Post('/signin')
--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
@@ -19,9 +20,30 @@ import { UserEntity } from './user.entity';
       },
     }),
     TypeOrmModule.forFeature([UserEntity]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:29092'],
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    {
+      provide: 'KAFKA_PRODUCER',
+      useFactory: async (kafkaService: ClientKafka) => {
+        return kafkaService.connect();
+      },
+      inject: ['KAFKA_SERVICE'],
+    }
+  ],
   exports: [JwtStrategy, PassportModule, AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
