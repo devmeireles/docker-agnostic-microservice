7686ec69de5898cfe85a92d32220d4ef54762e65
Gabriel Santos Meireles <dev.meireles@gmail.com>
2023-03-02 17:15:43 +0000

    updating seeds

--- database/seed.sql
+++ database/seed.sql
@@ -9,12 +9,15 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS products (
   id SERIAL NOT NULL,
   name varchar(64) NOT NULL,
+  price DOUBLE PRECISION NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO
-    products (name)
-VALUES
-    ('New cozy shoes'),
-    ('Modern smartphone'),
-    ('Green Basketball');
+INSERT INTO public.products(
+	name, price)
+	VALUES ('Super Skate', 75.30),
+	('Soccer Ball', 15.10),
+	('Bubble gum', 5.70),
+  ('New cozy shoes', 11.90),
+  ('Modern smartphone', 22),
+  ('Green Basketball', 5.55);
